abb224ca1fcb2093ba4a77927a1bee4db07eb1e4
SmallLeaf233 <2104448672@qq.com>
2022-08-10 13:14:46 +0800

    使用ttkbootstrap美化包

--- GUI.py
+++ GUI.py
@@ -1,11 +1,12 @@
 import os
 import time
-from tkinter import *
+import random
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 from tkinter import messagebox
 
 
-class Application(Frame):
-
+class Application(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -14,47 +15,59 @@ class Application(Frame):
 
     def createWidget(self):
         # 输入ip
-        self.labelIp = Label(self, text="本地IP")
-        self.labelIp.grid(row=0, column=0, sticky=SW)
-        self.ip = StringVar()
-        self.entryIp = Entry(self, textvariable=self.ip)
-        self.entryIp.grid(row=1, column=0)
+        self.labelIp = ttk.LabelFrame(self, text="本地IP")
+        self.labelIp.grid(row=0, column=0, sticky=W, pady=20)
+        self.ip = ttk.StringVar()
+        self.entryIp = ttk.Entry(self.labelIp, textvariable=self.ip, width=14, font=("黑体", 20), bootstyle="light")
+        self.entryIp.pack()
         self.ip.set("127.0.0.1")
 
         # 输入端口
-        self.labelPort = Label(self, text="端口")
-        self.labelPort.grid(row=0, column=1, sticky=SW)
-        self.port = StringVar()
-        self.entryPort = Entry(self, textvariable=self.port)
-        self.entryPort.grid(row=1, column=1)
+        self.labelPort = ttk.LabelFrame(self, text="端口")
+        self.labelPort.grid(row=0, column=0, sticky=E, pady=20)
+        self.port = ttk.StringVar()
+        self.entryPort = ttk.Entry(self.labelPort, textvariable=self.port, width=9, font=("黑体", 20), bootstyle="light")
+        self.entryPort.pack()
 
         # 隧道名称
-        self.labelName = Label(self, text="隧道名称")
-        self.labelName.grid(row=2, column=0, sticky=SW)
-        self.name = StringVar()
-        self.entryName = Entry(self, textvariable=self.name)
-        self.entryName.grid(row=3, column=0)
+        self.labelName = ttk.LabelFrame(self, text="隧道名称")
+        self.labelName.grid(row=1, column=0, sticky=E)
+        self.name = ttk.StringVar()
+        self.entryName = ttk.Entry(self.labelName, textvariable=self.name, width=25, font=("黑体", 20), bootstyle="light")
+        self.entryName.pack()
 
         # 隧道类型
-        self.labeltyp = Label(self, text="隧道类型")
-        self.labeltyp.grid(row=2, column=1, sticky=SW)
-        self.typ = StringVar(self)
+        self.labeltyp = ttk.LabelFrame(self, text="隧道类型")
+        self.labeltyp.grid(row=2, column=0, sticky=W, pady=20)
+        self.typ = ttk.StringVar()
         self.typ.set("TCP")
-        self.om01 = OptionMenu(self, self.typ, "TCP", "UDP")
-        self.om01.grid(row=3, column=1)
+        self.om01 = ttk.Combobox(self.labeltyp, textvariable=self.typ, value=("TCP", "UDP"),
+                                 width=8, font=("黑体", 20), state="readonly", bootstyle="light")
+        self.om01.pack()
 
-        self.btn01 = Button(self, text="创建", command=self.establish)
-        self.btn01.grid(row=4, column=0, sticky=EW)
-        self.btn02 = Button(self, text="启动", command=self.run)
-        self.btn02.grid(row=4, column=1, sticky=W)
-        self.btn03 = Button(self, text="停止", command=self.end)
-        self.btn03.grid(row=4, column=1, sticky=E)
+        # 远程端口
+        self.labelRemote = ttk.LabelFrame(self, text="远程端口(6000-6999 留空随机)")
+        self.labelRemote.grid(row=2, column=0,  sticky=E, pady=20)
+        self.remote = ttk.StringVar()
+        self.entryRemote = ttk.Entry(self.labelRemote, textvariable=self.remote, width=14, font=("黑体", 20), bootstyle="light")
+        self.entryRemote.pack()
+
+        s = ttk.Style()
+        s.configure('my.TButton', font=("黑体", 15))
+        self.btn01 = ttk.Button(self, text="创建", style='my.TButton', command=self.establish)
+        self.btn01.grid(row=3, column=0, sticky=NSEW, pady=20)
+        self.btn02 = ttk.Button(self, width=10, text="启动", style='my.TButton', command=self.run)
+        self.btn02.grid(row=4, column=0, sticky=W, padx=50, pady=20)
+        self.btn03 = ttk.Button(self, width=10, text="停止", style='my.TButton', command=self.end)
+        self.btn03.grid(row=4, column=0, sticky=E, padx=50, pady=20)
 
     def establish(self):
         if self.typ.get() == "TCP":
             typ = "tcp"
         if self.typ.get() == "UDP":
             typ = "udp"
+        if self.remote.get() == "":
+            self.remote.set(random.randint(6000, 6999))  # 随机数生成，服务器的端口我只开放了这个范围，应该够了
         with open("frpc.ini", "w") as f:
             list = ['[common]\n',
                     'server_addr = 【这里填服务器IP】\n',
@@ -66,41 +79,40 @@ class Application(Frame):
                     'type = ' + typ + '\n',
                     'local_ip = ' + self.ip.get() + '\n',
                     'local_port = ' + self.port.get() + '\n',
-                    'remote_port = 6002']
+                    'remote_port = ' + self.remote.get()]
             f.writelines(list)
 
     def run(self):
         os.popen('frpc.exe')
-        time.sleep(0.5)
+        time.sleep(0.5)  # 睡眠0.5秒
         with open("frpc.log", "r") as f:
             self.log = str(f.readlines()[-3:])
-            print(self.log)
             if "[W]" in self.log:
                 os.system('taskkill /F /IM frpc.exe')
                 if "port already" in self.log:
-                    messagebox.showerror("启动失败", "远程端口已被使用，请改用其他端口\n\n程序已禁用自选远程端口功能，请联系小叶子")
+                    messagebox.showerror("启动失败", "远程端口已被使用，请改用其他端口")
                 elif "proxy name" in self.log:
                     messagebox.showerror("启动失败", "隧道名称" + self.name.get() + "已被使用，请检查并更换一个隧道名称")
                 else:
-                    messagebox.showerror("启动失败", "出现了没有预料的错误，请检查日志文件frpc.log，将日志文件发给小叶子")
+                    messagebox.showerror("启动失败", "出现了没有预料的错误，请检查日志文件frpc.log，联系小叶子")
             else:
                 messagebox.showinfo("启动成功", "您的本地地址：" + self.ip.get() + ":" + self.port.get() +
-                                    "已映射到服务器地址：【这里填服务器IP】:6001")
+                                    "已映射到服务器地址：【这里填服务器IP】:" + self.remote.get())
 
     def end(self):
-        os.system('chcp 65001')
-        os.system('taskkill /F /IM frpc.exe')
+        os.popen('taskkill /F /IM frpc.exe')
+        messagebox.showerror("停止", "已结束frpc.exe进程")
 
 
 if __name__ == '__main__':
-    root = Tk()
-    root.geometry("300x150+600+250")
+    root = ttk.Window(themename="minty")
+    root.geometry("600x500+600+250")
     root.title("FRPGUI")
     app = Application(master=root)
 
 
     def end():
-        os.system('taskkill /F /IM frpc.exe')
+        os.popen('taskkill /F /IM frpc.exe')
         root.destroy()
 
 
